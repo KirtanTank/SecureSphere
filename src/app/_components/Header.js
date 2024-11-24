b444dec0009661cd2bfb0f90fcc743897ce7eff6
Kirtan Tank <tankkirtan5@gmail.com>
2024-11-25 01:14:09 +0530

    defining components and font removal

--- src/app/_components/Header.js
+++ src/app/_components/Header.js
@@ -6,8 +6,8 @@ const Header = () => {
           <span className="ml-3 text-xl">Tailblocks</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">Add User</a>
-          <a className="mr-5 hover:text-gray-900">Add Role</a>
+          <button className="mr-5 hover:text-gray-900">Add User</button>
+          <button className="mr-5 hover:text-gray-900">Add Role</button>
         </nav>
         <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
           Login
